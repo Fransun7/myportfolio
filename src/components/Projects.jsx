7fb3b7203ee650927854519d37bfa788c879dae0
Fransun7 <fransunlight777@gmail.com>
2025-09-05 16:03:31 +0100

    updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,52 +8,50 @@ function Projects() {
       </p>
       {/* Project section */}
       <div
-        className="bg-no-repeat bg-cover bg-center mt-6 w-screen grid grid-cols-2 gap-20 p-3 sm:grid-cols-3 md:grid-cols-4"
+        className="bg-no-repeat bg-cover bg-center mt-6  grid grid-cols-2 gap-20 sm:grid-cols-3 sm:gap-6 md:grid-cols-4 md:gap-9 w-screen overflow-auto p-2 justify-center"
         style={{ backgroundImage: `url(${ProjectBg})` }}
       >
-        <div class="w-60 h-80 bg-neutral-800 rounded-3xl text-neutral-300 p-4 flex flex-col items-start justify-center gap-3 hover:bg-gray-900 hover:shadow-2xl hover:shadow-sky-400 transition-shadow">
-          <div class="w-52 h-40 bg-sky-300 rounded-2xl"></div>
-          <div class="">
-            <p class="font-extrabold">Card title</p>
-            <p class="">4 popular types of cards in UI design.</p>
-          </div>
-          <button class="bg-sky-700 font-extrabold p-2 px-6 rounded-xl hover:bg-sky-500 transition-colors">
-            See more
-          </button>
-        </div>
 
-        <div class="w-60 h-80 bg-neutral-800 rounded-3xl text-neutral-300 p-4 flex flex-col items-start justify-center gap-3 hover:bg-gray-900 hover:shadow-2xl hover:shadow-sky-400 transition-shadow">
-          <div class="w-52 h-40 bg-sky-300 rounded-2xl"></div>
-          <div class="">
-            <p class="font-extrabold">Card title</p>
-            <p class="">4 popular types of cards in UI design.</p>
-          </div>
-          <button class="bg-sky-700 font-extrabold p-2 px-6 rounded-xl hover:bg-sky-500 transition-colors">
-            See more
-          </button>
-        </div>
+<div class="w-50 h-70 bg-neutral-800 rounded-2xl text-neutral-300 p-2 flex flex-col items-start  justify-center gap-3 hover:bg-gray-900 hover:shadow-2xl hover:shadow-sky-400 transition-shadow">
+  <div class="w-46 h-33 bg-sky-300 rounded-2xl"></div>
+  <div class="">
+      <p class="font-extrabold">Card title</p>
+      <p class="">4 popular types of cards in UI design.</p>
+  </div>
+  <button class="bg-sky-700 font-extrabold p-2 px-5 rounded-xl hover:bg-sky-500 transition-colors">See more</button>
+</div>
 
-        <div class="w-60 h-80 bg-neutral-800 rounded-3xl text-neutral-300 p-4 flex flex-col items-start justify-center gap-3 hover:bg-gray-900 hover:shadow-2xl hover:shadow-sky-400 transition-shadow mt-2">
-          <div class="w-52 h-40 bg-sky-300 rounded-2xl"></div>
-          <div class="">
-            <p class="font-extrabold">Card title</p>
-            <p class="">4 popular types of cards in UI design.</p>
-          </div>
-          <button class="bg-sky-700 font-extrabold p-2 px-6 rounded-xl hover:bg-sky-500 transition-colors">
-            See more
-          </button>
-        </div>
+<div class="w-50 h-70 bg-neutral-800 rounded-2xl text-neutral-300 p-2 flex flex-col items-start  justify-center gap-3 hover:bg-gray-900 hover:shadow-2xl hover:shadow-sky-400 transition-shadow">
+  <div class="w-46 h-33 bg-sky-300 rounded-2xl"></div>
+  <div class="">
+      <p class="font-extrabold">Card title</p>
+      <p class="">4 popular types of cards in UI design.</p>
+  </div>
+  <button class="bg-sky-700 font-extrabold p-2 px-5 rounded-xl hover:bg-sky-500 transition-colors">See more</button>
+</div>
 
-        <div class="w-60 h-80 bg-neutral-800 rounded-3xl text-neutral-300 p-4 flex flex-col items-start justify-center gap-3 hover:bg-gray-900 hover:shadow-2xl hover:shadow-sky-400 transition-shadow mt-2">
-          <div class="w-52 h-40 bg-sky-300 rounded-2xl"></div>
-          <div class="">
-            <p class="font-extrabold">Card title</p>
-            <p class="">4 popular types of cards in UI design.</p>
-          </div>
-          <button class="bg-sky-700 font-extrabold p-2 px-6 rounded-xl hover:bg-sky-500 transition-colors">
-            See more
-          </button>
-        </div>
+<div class="w-50 h-70 bg-neutral-800 rounded-2xl text-neutral-300 p-2 flex flex-col items-start  justify-center gap-3 hover:bg-gray-900 hover:shadow-2xl hover:shadow-sky-400 transition-shadow">
+  <div class="w-46 h-33 bg-sky-300 rounded-2xl"></div>
+  <div class="">
+      <p class="font-extrabold">Card title</p>
+      <p class="">4 popular types of cards in UI design.</p>
+  </div>
+  <button class="bg-sky-700 font-extrabold p-2 px-5 rounded-xl hover:bg-sky-500 transition-colors">See more</button>
+</div>
+
+<div class="w-50 h-70 bg-neutral-800 rounded-2xl text-neutral-300 p-2 flex flex-col items-start  justify-center gap-3 hover:bg-gray-900 hover:shadow-2xl hover:shadow-sky-400 transition-shadow">
+  <div class="w-46 h-33 bg-sky-300 rounded-2xl"></div>
+  <div class="">
+      <p class="font-extrabold">Card title</p>
+      <p class="">4 popular types of cards in UI design.</p>
+  </div>
+  <button class="bg-sky-700 font-extrabold p-2 px-5 rounded-xl hover:bg-sky-500 transition-colors">See more</button>
+</div>
+
+
+
+
+
       </div>
     </div>
   );
