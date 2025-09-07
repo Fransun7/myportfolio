4fc95370566b59101be5d4eda341b30fe7399655
Fransun7 <fransunlight777@gmail.com>
2025-09-07 20:05:04 +0100

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,31 +3,52 @@ import emailjs from "@emailjs/browser";
 import ContactBg1 from "../assets/Images/Contact-bg1.jpg";
 import ContactBg2 from "../assets/Images/Contact-bg2.jpg";
 import ContactBg3 from "../assets/Images/Contact-bg3.jpg";
+import { sendForm } from "emailjs-com";
 
 function Contact() {
-  const form = useRef();
+  // const form = useRef();
 
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+
+  //   emailjs
+  //     .sendForm(
+  //       "service_2tw97p7",
+  //       "template_lumpw1i",
+  //       form.current,
+  //       "Qng7D-nvLnXuYz-b7"
+  //     )
+  //     .then(
+  //       (result) => {
+  //         console.log("SUCCESS!", result.text);
+  //         alert("Message sent successfully!");
+  //         e.target.reset();
+  //       },
+  //       (error) => {
+  //         console.log("FAILED...", error.text);
+  //         alert("Failed to send message. Please try again.");
+  //       }
+  //     );
+  // };
+
+  const Form = useRef();
   const sendEmail = (e) => {
-    e.preventDefault(); // Prevents the form from submitting in the traditional way
+    e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_z2uk4uh",
-        "template_ca0ay1k",
-        form.current,
-        "Qng7D-nvLnXuYz-b7"
-      )
-      .then(
-        (result) => {
-          console.log("SUCCESS!", result.text);
-          alert("Message sent successfully!"); // Provide feedback to the user
-          e.target.reset(); // Clear the form fields
-        },
-        (error) => {
-          console.log("FAILED...", error.text);
+    emailjs.sendForm (
+      "service_2tw97p7",
+        "template_lumpw1i",
+      Form.current,
+      "Qng7D-nvLnXuYz-b7"
+    )
+    .then ((result) => {
+      console.log("SUCCESS!", result.text);
+          alert("Message sent successfully!");
+          e.target.reset();
+    }, (error) => {
+      console.log("FAILED...", error.text);
           alert("Failed to send message. Please try again.");
-        }
-      );
+    })
   };
 
   return (
@@ -36,7 +57,7 @@ function Contact() {
       style={{ backgroundImage: `url(${ContactBg3})` }}
     >
       <p className="justify-self-center font-story-script text-2xl  ">
-        - About me -
+        - Contact -
       </p>
 
       <div class="container px-4 mx-auto p-20">
@@ -45,7 +66,7 @@ function Contact() {
             <h2 class="text-2xl font-semibold text-cyan-600 mb-4">
               Contact Us
             </h2>
-            <form ref={form} onSubmit={sendEmail}>
+            <form ref={Form} onSubmit={sendEmail}>
               <div class="mb-4">
                 <label class="block text-cyan-800 mb-1" for="name">
                   Your Name
@@ -78,6 +99,7 @@ function Contact() {
                   class="w-full px-4 py-2 bg-neutral-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-600 transition duration-300"
                   placeholder="Enter job title"
                   type="text"
+                  name="subject"
                 />
               </div>
               <div class="mb-4">
@@ -98,6 +120,12 @@ function Contact() {
               >
                 Send Message
               </button>
+              <div className="flex items-center justify-center">
+                <a href="https://wa.me/2349137929760?text=Hello%20I%20am%20interested%20in%20your%20services" target="_blank" // Opens the link in a new tab
+                rel="noopener noreferrer" // Security best practice
+                className="inline-flex items-center text-cyan-600 hover:text-cyan-400 transition-colors duration-300 font-mono"> <span>You can also contact me on <span className="text-white hover:text-cyan-900">Whatsapp</span></span>
+                </a>
+              </div>
             </form>
           </div>
         </div>
